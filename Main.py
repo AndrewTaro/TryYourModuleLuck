cdf1947d0de7735bada423492e70918856d51656
AndrewTaro <36262823+AndrewTaro@users.noreply.github.com>
2023-05-05 03:57:02 +0900

    fixed dict order

--- Main.py
+++ Main.py
@@ -1,5 +1,12 @@
 import random, json
 
+#Order is IMPORTANT
+GUN_COMPONENTS = ['artillery', 'torpedoes', 'atba', 'airDefense', 'depthCharges', 'chargeLasers', 'impulseLasers', 'axisLaser', 'waves', 'pinger']
+OTHER_COMPONENTS = ['airArmament', 'engine']
+HULL_COMPONENTS = ['steering_gear_hitlocation', 'simple_hitlocation', 'citadel_hitlocation', 'casemate_hitlocation', 'supersctructure_hitlocation', 'powdermagazine_hitlocation', 'hull_hitlocation']
+
+COMPONENTS = GUN_COMPONENTS + OTHER_COMPONENTS
+
 class HitLocation(object):
     def __init__(self, max_hp, rnd_part_hp):
         self.max_hp = max_hp
@@ -57,15 +64,27 @@ def get_components(ship):
     hulls[-1]['components']['engine'] = engines[-1]['components']['engine']
     return hulls[-1]['components']
 
+
+def get_component(components, hl_type):
+    for comp_name, comp in components.iteritems():
+        if not isinstance(comp, dict) or 'hlType' not in comp:
+        #if not hasattr(comp, 'hlType'):
+            continue
+        if comp['hlType'] != hl_type:
+            continue
+        yield (comp_name, comp)
+
 def get_hitlocations(ship):
     hit_locations = {}
     components = get_components(ship)
 
-    for comps in components.itervalues():
-        if len(comps) >= 1:
-            comp_name = comps[-1]
-        else:
+    for _name in COMPONENTS:
+        comps = components.get(_name)
+        if comps is None or len(comps) == 0:
             continue
+        comp_name = comps[-1]
+
+        #print(comp_name)
 
         for module_name, module in ship[comp_name].iteritems():
             hit_loc = get_hitlocation(module_name, module)
@@ -76,23 +95,34 @@ def get_hitlocations(ship):
 
             hit_locations[name] = HitLocation(hl['maxHP'], hl['rndPartHP'])
 
+    hull = components['hull'][-1]
+    for _name in HULL_COMPONENTS:
+        for comp_name, comp in get_component(ship[hull], _name):
+            hit_locations[comp_name] = HitLocation(comp['maxHP'], comp['rndPartHP'])
+
     return hit_locations
 
 
-SHIP_NAME = 'PRSC610_Smolensk'
-ENTITY_ID = 405147
+SHIP_NAME = 'PFSD110_Kleber'
+ENTITY_ID = 811267
 
 def main():
     filename = 'Ship\\{}.json'.format(SHIP_NAME)
     with open(filename, 'r') as f:
         ship = json.load(f)
+    #with open('all_gameparams.json', 'r') as f:
+    #    gp = json.load(f)
+    #gp = gp[0]
+
+    #ship = gp.get(SHIP_NAME)
 
     hit_locations = get_hitlocations(ship)
 
     try_luck(ENTITY_ID, hit_locations)
 
-    for hl_name, hl in hit_locations.items():
+    for hl_name, hl in hit_locations.iteritems():
         print(hl_name, hl)
+
 
 if __name__ == '__main__':
     main()
